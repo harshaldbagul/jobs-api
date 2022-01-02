bd0f8e158a2721a1d1084af974ef56558bfe2052
Harshal Bagul <HarshalB@talentica-all.com>
2022-01-02 18:21:27 +0530

    added testing route

--- src/app.js
+++ src/app.js
@@ -32,6 +32,7 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/',(req,res)=>res.send('jobs-api'));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', auth, jobsRouter);
