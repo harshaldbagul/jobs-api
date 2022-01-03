d357b176c86d4890a73596bb752f15828402cc6b
Harshal Bagul <HarshalB@talentica-all.com>
2022-01-03 12:05:01 +0530

    added swagger docs

--- src/app.js
+++ src/app.js
@@ -32,7 +32,15 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get('/',(req,res)=>res.send('jobs-api'));
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', auth, jobsRouter);
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -13,7 +13,6 @@ const auth = async (req, res, next) => {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findOne({ _id: payload.userId }).select('-password');
         req.user = user;
-        console.log(user);
         next();
     } catch (er) {
         throw new UnauthenticatedError('Invalid token');
